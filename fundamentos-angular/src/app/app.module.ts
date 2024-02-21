80407d7380829ecc71dea8d97efd4d1710be4210
Natalia Barros <natalia.vieiramonteiro70@gmail.com>
2024-02-21 19:48:16 -0300

    pipe

--- fundamentos-angular/src/app/app.module.ts
+++ fundamentos-angular/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
 import { SegundoComponentComponent } from './segundo-component/segundo-component.component';
+import { MultiplicaPorPipe } from './multiplica-por.pipe';
+import { PipeCustomizadoComponent } from './pipe-customizado/pipe-customizado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiroComponenteComponent,
-    SegundoComponentComponent
+    SegundoComponentComponent,
+
+    MultiplicaPorPipe,
+     PipeCustomizadoComponent,
+
   ],
   imports: [
     BrowserModule,
